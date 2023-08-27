9da2cbcbb8336a7fe166548ea5b06bf195d29dff
abhisekhgupta10 <abhisekhgupta2020@gmail.com>
2023-08-27 22:12:57 +0545

    added edit and delete action

--- src/Pages/ContactPage/Contactpage.jsx
+++ src/Pages/ContactPage/Contactpage.jsx
@@ -6,21 +6,50 @@ const Contactpage = () => {
     name:"",
     email:""
   })
-
+  
+  const[editClicked,setClicked] = useState(false)
+  const[editIndex,setEditIndex]= useState("")
   const[tabData,settabData] = useState([])
 
   const handlechange = (e)=>{
    const name = e.target.name
    const value = e.target.value
    setFormData(values=>({...values,[name]:value}))
-   console.log(formData)
+ 
   }
 
   const handlesubmit =(e)=>{
     e.preventDefault()
     console.log(formData)
-   settabData((data)=>([...data, formData]))
-    console.log(tabData)
+
+    if(editClicked){
+      const tempTabData =tabData
+      Object.assign(tempTabData[editIndex],formData)
+      settabData([...tempTabData])
+    }else{
+      settabData((data)=>([...data, formData]))
+      setFormData({
+        name:"",
+        email:""
+      })
+    }
+ 
+  }
+
+  const edit = (i)=>{
+    setClicked(true)
+    const tempData = tabData[i]
+   
+    setFormData({name:tempData.name,email:tempData.email})
+    setEditIndex(i)
+    
+  }
+
+  const delet = (i)=>{
+    const filtered = tabData.filter((item,index)=>{
+      return index!==i
+    })
+    settabData(filtered)
   }
   return (
 <>
@@ -34,7 +63,9 @@ const Contactpage = () => {
     <label htmlFor="email">Email</label>
     <input type="email" className='form-control' value={formData.email} name="email" id='email' onChange={(e)=>handlechange(e)}/>
   </div>
- <button className='btn btn-lg btn-primary'>Submit</button>
+ <button className='btn btn-lg btn-primary'>
+  {editClicked?"Update":"Submit"}
+ </button>
 </form>
 
 <div className="mt-4 container">
@@ -44,16 +75,18 @@ const Contactpage = () => {
       <tr>
       <th scope='col'>Name</th>
       <th scope="col">Email</th>
+      <th scope='col'>Action</th>
     </tr>
     </thead>
     <tbody>
     {
   
-  tabData&&(tabData.map((data,i)=>{
+  tabData&&(tabData.map((data,index)=>{
     return<>
-  <tr key={i}>
+  <tr key={index}>
     <td scope='row'>{data.name}</td>
     <td>{data.email}</td>
+    <td><button className='btn btn-outline-primary' onClick={()=>edit(index)}>Edit</button> <button onClick={()=>delet(index)} className='btn btn-outline-danger'>Delete</button></td>
   </tr>
 
     </>
